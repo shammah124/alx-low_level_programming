03e057636dfa1b636fcc21f29c47290606390665
Shammah Ayuba <shammahbagat@yahoo.com>
2022-09-09 14:24:46 -0700

    Low Level Programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int dd;
+	int n;
 
 	srand(time(0));
-	dd = rand() - RAND_MAX / 2;
-	if (dd > 0)
-		printf("%d is positive\n", dd);
-	else if (dd == 0)
-		printf("%d is zero\n", dd);
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-		printf("%d is negative\n", dd);
+		printf("%d is negative\n", n);
 	return (0);
 }
