7a6a6abaa93da18cc0fdb2f4781d01a4f054d36e
Shammah Ayuba <shammahbagat@yahoo.com>
2022-09-13 18:19:16 -0700

    C

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -10,15 +10,3 @@ int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-/**
-* print_alphabet - print void
-* return 0 upon success
-*
-* void print_alphabet(void)
-* {
-* return (write(void));
-* }
-*/
