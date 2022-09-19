9979231836d7a1a4863fc97ae94daaf0fe654422
Shammah Ayuba <shammahbagat@yahoo.com>
2022-09-19 07:00:29 -0700

    A pointer to an integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* reset_to_98 - function that takes a pointer to an int parameter
+* @x: integer input pointer
+* Return: Nothing.
+*/
+void reset_to_98(int *x)
+{
+
+	*x = 98;
+
+}
+
